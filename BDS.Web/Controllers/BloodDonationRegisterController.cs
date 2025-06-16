f5e73671b827f42b40fb1d456142268bf878a04e
hwthinhj <127239880+nptror@users.noreply.github.com>
2025-06-16 17:05:10 +0700

    Reference các project

---
+++ BDS.Web/Controllers/BloodDonationRegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BDS.Web.Controllers
+{
+    public class BloodDonationRegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
