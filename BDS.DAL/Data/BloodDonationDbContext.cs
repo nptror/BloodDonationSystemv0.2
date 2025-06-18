4d94fc7df1de32485cdd7a6f101cc6057b0f7ba0
hwthinhj <127239880+nptror@users.noreply.github.com>
2025-06-19 02:11:21 +0700

    refactor : - allow user input register donation day - fix getconnection appsetting (error cant create register donation)

--- BDS.DAL/Data/BloodDonationDbContext.cs
+++ BDS.DAL/Data/BloodDonationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using BDS.DAL.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BDS.DAL.Data;
 
@@ -34,8 +35,18 @@ public partial class BloodDonationDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-JBC3UOF\\MSSQLSERVER01;Database=TestSystem;User Id=sa;Password=12345;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
 
+    private string GetConnectionString()
+    {
+        IConfiguration config = new ConfigurationBuilder()
+             .SetBasePath(AppContext.BaseDirectory)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .Build();
+        var strConn = config["ConnectionStrings:cnn"];
+
+        return strConn;
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<BloodCompatibility>(entity =>
