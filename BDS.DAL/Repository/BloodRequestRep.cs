0630171164e906f17762e34f74b829e6dfcdd45d
hwthinhj <127239880+nptror@users.noreply.github.com>
2025-06-22 22:57:38 +0700

    test: pass

---
+++ BDS.DAL/Repository/BloodRequestRep.cs
@@ -0,0 +1,28 @@
+﻿using BDS.Common.DAL;
+using BDS.DAL.Data;
+using BDS.DAL.Models;
+
+namespace BDS.DAL.Repository
+{
+    public class BloodRequestRep : GenericRep<BloodDonationDbContext, BloodRequest>
+    {
+        public BloodRequestRep()
+        {
+            // Constructor logic if needed
+        }
+
+        public new void Create(BloodRequest m)
+        {
+            if (m == null)
+            {
+                throw new ArgumentNullException(nameof(m), "BloodRequest cannot be null");
+            }
+            base.Create(m);
+        }
+
+        //public override List<BloodDonationRegister> Read() 
+        //{
+        
+        //}
+    }
+}
